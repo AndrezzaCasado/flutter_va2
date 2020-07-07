b434ae29ad68ebed62df7808d87b1176d7f23b73
Raul-Duarte <raul.costa@gespag.net.br>
2020-07-07 18:43:32 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_va2/ui/listview_product.dart';
+
+void main() => runApp(new MaterialApp(
+  home: ListViewProduct(),
+  title: 'Product Db',
+));
